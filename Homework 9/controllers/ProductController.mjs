4b02d4bcfbae6203b41634d702e8ed7892d75c8e
DanielOremus <oremusd89@gmail.com>
2024-11-12 18:35:27 +0200

    finished task 1

--- Homework 9/controllers/ProductController.mjs
+++ Homework 9/controllers/ProductController.mjs
@@ -1,14 +1,47 @@
+import { validationResult } from "express-validator"
 import ProductManager from "../models/product/ProductManager.mjs"
 
 class ProductController {
   static async loadList(req, res) {
     try {
-      const products = await ProductManager.getList({}, { sort: { price: -1 } })
-      console.log(products)
+      const products = await ProductManager.getList(
+        {},
+        { sort: { price: req.session.sortInAscOrder ? 1 : -1 } }
+      )
 
       res.render("product/list", {
         metaTitle: "Products List",
         products,
+      })
+    } catch (error) {
+      res.status(500).json({ success: false, msg: error.message })
+    }
+  }
+  static async create(req, res) {
+    const { title, price, amount } = req.body
+    const product = { title, price, amount }
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).render("product/form", {
+        metaTitle: "Form",
+        product,
+        errors: errors.array(),
+      })
+    }
+    try {
+      await ProductManager.create({ title, price, amount })
+      req.session.sortInAscOrder = false
+      res.redirect("/products")
+    } catch (error) {
+      res.status(500).json({ success: false, msg: error.message })
+    }
+  }
+  static async renderForm(req, res) {
+    try {
+      res.render("product/form", {
+        metaTitle: "Form",
+        errors: [],
+        product: null,
       })
     } catch (error) {
       res.status(500).json({ success: false, msg: error.message })
--- Homework 9/routes/products.mjs
+++ Homework 9/routes/products.mjs
@@ -1,9 +1,20 @@
 import { Router } from "express"
 import isAuthorized from "../middlewares/isAuthorized.mjs"
 import ProductController from "../controllers/ProductController.mjs"
+import { checkSchema } from "express-validator"
+import ProductValidator from "../validators/ProductValidator.mjs"
 
 const router = Router()
 
 router.get("/", isAuthorized, ProductController.loadList)
+
+router.get("/addProduct", isAuthorized, ProductController.renderForm)
+
+router.post(
+  "/addProduct",
+  isAuthorized,
+  checkSchema(ProductValidator.schema),
+  ProductController.create
+)
 
 export default router
